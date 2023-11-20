8ebbfdad418f05e0ec9bad7a6c87173709c81b55
Pedro <kapoty1@gmail.com>
2023-11-20 16:29:31 -0300

    out

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     	"app": "./src/App.jsx"
     },
     "output": {
-        "path": __dirname+'/out',
+        "path": __dirname+'/docs',
         "filename": "[name].[chunkhash:8].js"
     },
     "module": {
